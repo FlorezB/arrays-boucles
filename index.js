bb4dc08fee45e855d7827900d6d45d39654514b8
Caroline <alyseflorez@hotmail.fr>
2022-06-27 15:00:07 +0200

    exo arrays

---
+++ index.js
@@ -0,0 +1,87 @@
+// 01
+// const fruits = ["mango" , "lemon" , "blueberry"]
+// console.log(fruits)
+// console.table(fruits)
+
+
+// 02
+// const ingredients = ["eggs" , "milk" , "butter"]
+// console.log (ingredients [1]);
+// console.log (ingredients [2]);
+//ingredients.indexOf("butter");
+
+
+//03
+// const objects = ["pen" , "book" , "chair"]
+// objects.unshift("chair")
+// objects.pop()
+// objects.push("laptop")
+// objects.shift("chair")
+// console.log(objects)
+
+
+//04
+// const numbers = [4 , 10 , 8 , 12 , 6]
+// numbers.reverse()
+// console.table(numbers)
+
+
+//05
+// let total = 0
+// const limit = 10
+// for (let i = 0; i <= limit; i++) {
+// total = total +i
+// console.log(total) 
+// }
+
+
+//06
+// const sentence = ("Hello Konexio !")
+
+
+// for (let i = sentence.length -1; i >=0; i--) {
+
+//     console.log (sentence.charAt(i))
+// }
+
+
+// 07
+// for (let i = 1; i<=100; i++) {
+
+//     if (i%3 && i%5 == 0) {
+//         console.log("fizzbuzz")
+//     }
+//     else if (i%3==0) {
+//         console.log("fizz")
+//     }
+//     else if (i%5==0) {
+//         console.log("buzz")
+//     }
+//     else if (i%7!==0) {
+//         console.log("i")
+//     }
+// }
+
+
+//08
+// let total = 0
+// const limit = 10
+// let i = 1
+// while (i <= limit) {
+// total = total+i
+// i++
+// console.log(total)
+// }
+
+
+// 09
+// let camarades = ["Walid","Wade","Rayan","Walid","Benoit","Emad","Jad","Lynda","Florez","Lucas","Arthur","Thomas","Aurelien","Clara","Danh","Cinnie","Edaly","Mago","Dushen","Julien","Florent"];
+
+// let x = Math.floor(Math.random() * 21)
+// console.log(camarades[x])
+
+
+// 10
+// let vide = []
+// for (let i = 1; i < 100; i++)
+// console.log(vide)
